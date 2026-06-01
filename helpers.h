b929771f38bc1e3ad1013425fdb2a0dea423dfa3
baraiksahil <baraiksahilkumar@gmail.com>
2026-06-02 00:51:12 +0530

    Added the wall collision

--- helpers.h
+++ helpers.h
@@ -1,48 +1,125 @@
 #include <iostream>
 #include <unistd.h>
 
-void drawWall()
+#ifdef _WIN32
+#include <conio.h>
+#else
+#include <termios.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+// Custom implementation of _kbhit() for macOS/Linux
+int _kbhit()
 {
-    int xWidth = 200;
-    int yHeight = 40;
+    struct termios oldt, newt;
+    int ch;
+    int oldf;
 
-    int xSnake = 30;
-    int ySnake = 15;
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
 
-    while (true)
+    ch = getchar();
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+    if (ch != EOF)
     {
-        system("cls");
-        for (int y = 0; y < yHeight; y++)
-        {
-            for (int x = 0; x < xWidth; x++)
-            {
-                // Snake
-                if (x == xSnake && y == ySnake)
-                {
-                    std::cout << "O";
-                }
-
-                // Walls
-                else if (
-                    x == 0 ||
-                    x == xWidth - 1 ||
-                    y == 0 ||
-                    y == yHeight - 1)
-                {
-                    std::cout << "#";
-                }
-
-                // Empty space
-                else
-                {
-                    std::cout << " ";
-                }
-            }
-
-            std::cout << "\n";
-        }
-
-        xSnake++;
-        usleep(100000);
+        ungetc(ch, stdin);
+        return 1;
     }
+
+    return 0;
+}
+
+// Custom implementation of _getch() for macOS/Linux
+int _getch()
+{
+    struct termios oldt, newt;
+    int ch;
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    ch = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    return ch;
+}
+#endif
+
+int xWidth = 60; 
+int yHeight = 20;
+
+int xSnake = 30;
+int ySnake = 10;
+
+bool gameOver = false;
+
+char direction = 'D';
+
+void draw()
+{
+    // Use ANSI escape code to move cursor home instead of system("cls") to prevent flickering
+    std::string frame = "\033[H";
+
+    for (int y = 0; y < yHeight; y++)
+    {
+        for (int x = 0; x < xWidth; x++)
+        {
+            // Snake
+            if (x == xSnake && y == ySnake)
+            {
+                frame += "X";
+            }
+            // Walls
+            else if (x == 0 || x == xWidth - 1 || y == 0 || y == yHeight - 1)
+            {
+                frame += "#";
+            }
+            // Blank Space
+            else
+            {
+                frame += " ";
+            }
+        }
+        frame += "\n";
+    }
+    std::cout << frame << std::flush;
+}
+
+void input()
+{
+    if (_kbhit())
+    {
+        char key = _getch();
+        if (key == 'w')
+            direction = 'W';
+        if (key == 'a')
+            direction = 'A';
+        if (key == 's')
+            direction = 'S';
+        if (key == 'd')
+            direction = 'D';
+    }
+}
+
+void logic()
+{
+    // direction of snake
+    if (direction == 'D')
+        xSnake++;
+    if (direction == 'A')
+        xSnake--;
+    if (direction == 'S')
+        ySnake++;
+    if (direction == 'W')
+        ySnake--;
+
+    // wall collision
+    if (xSnake <= 0 || xSnake >= xWidth - 1 || ySnake <= 0 || ySnake >= yHeight - 1)
+        gameOver = true;
 }
--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
+#include <unistd.h>
 #include "helpers.h"
 
 int main()
 {
 
-    drawWall();
+    while (!gameOver)
+    {
+        draw();
+        input();
+        logic();
+        usleep(100000); 
+    }
+    // Clear screen
+    std::cout << "\033[2J\033[H";
+    std::cout << "Game Over!!!\n";
 
     return 0;
 }
