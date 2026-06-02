195bb0ef8de087e8120952e730857be770358c09
baraiksahil <baraiksahilkumar@gmail.com>
2026-06-02 13:31:55 +0530

    Completed the snake game

--- helpers.h
+++ helpers.h
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <unistd.h>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
 
 #ifdef _WIN32
-#include <conio.h>
 #else
 #include <termios.h>
 #include <fcntl.h>
@@ -51,29 +53,91 @@ int _getch()
 }
 #endif
 
-int xWidth = 60; 
+// Screen size
+int xWidth = 60;
 int yHeight = 20;
 
-int xSnake = 30;
+// Snake head position
+int xSnake = 20;
 int ySnake = 10;
 
+// Snake body
+std::vector<int> snakeX;
+std::vector<int> snakeY;
+
+// Food position
+int xFood;
+int yFood;
+
+// Game state
 bool gameOver = false;
 
+// Snake direction
 char direction = 'D';
+
+// Score
+int score = 0;
+
+// Speed (microseconds delay)
+int delay = 150000;
+
+void foodSpawn()
+{
+    // xFood = rand() % (xWidth - 2) + 1;
+    // yFood = rand() % (yHeight - 2) + 1;
+
+    bool insideSnake;
+
+    do
+    {
+        insideSnake = false;
+        xFood = rand() % (xWidth - 2) + 1;
+        yFood = rand() % (yHeight - 2) + 1;
+
+        // check all snake body parts
+        for (int i = 0; i < snakeX.size(); i++)
+        {
+            if (xFood == snakeX[i] && yFood == snakeY[i])
+            {
+                insideSnake = true;
+                break;
+            }
+        }
+    } while (insideSnake);
+}
 
 void draw()
 {
     // Use ANSI escape code to move cursor home instead of system("cls") to prevent flickering
     std::string frame = "\033[H";
 
+    // Score
+    frame += "Score: " + std::to_string(score) + "\n";
+
     for (int y = 0; y < yHeight; y++)
     {
         for (int x = 0; x < xWidth; x++)
         {
-            // Snake
-            if (x == xSnake && y == ySnake)
+            // draw snake body
+            bool isSnake = false;
+            for (int i = 0; i < snakeX.size(); i++)
+            {
+                if (x == snakeX[i] && y == snakeY[i])
+                {
+                    isSnake = true;
+                    break;
+                }
+            }
+
+            // Snake position
+            if (isSnake)
             {
                 frame += "X";
+            }
+            // Food
+            else if (x == xFood && y == yFood)
+            {
+                frame += "O";
             }
             // Walls
             else if (x == 0 || x == xWidth - 1 || y == 0 || y == yHeight - 1)
@@ -119,7 +183,37 @@ void logic()
     if (direction == 'W')
         ySnake--;
 
+    // add new head position
+    snakeX.insert(snakeX.begin(), xSnake);
+    snakeY.insert(snakeY.begin(), ySnake);
+
+    // food collision
+    if (xSnake == xFood && ySnake == yFood)
+    {
+        score += 10;
+        foodSpawn();
+
+        // SPEED INCREASE
+        if (delay > 50000)
+            delay -= 5000;
+    }
+    else
+    {
+        // remove tail
+        snakeX.pop_back();
+        snakeY.pop_back();
+    }
+
     // wall collision
     if (xSnake <= 0 || xSnake >= xWidth - 1 || ySnake <= 0 || ySnake >= yHeight - 1)
         gameOver = true;
+
+    // self collision
+    for (int i = 1; i < snakeX.size(); i++)
+    {
+        if (xSnake == snakeX[i] && ySnake == snakeY[i])
+        {
+            gameOver = true;
+        }
+    }
 }
--- main.cpp
+++ main.cpp
@@ -5,16 +5,28 @@
 int main()
 {
 
+    srand(time(0));
+
+    // Initial snake
+    snakeX.push_back(xSnake);
+    snakeY.push_back(ySnake);
+
+    foodSpawn();
+
+    // Clear screen once
+    std::cout << "\033[2J";
+
     while (!gameOver)
     {
         draw();
         input();
         logic();
-        usleep(100000); 
+        usleep(150000); // 150ms delay for standard normal gameplay speed
     }
     // Clear screen
     std::cout << "\033[2J\033[H";
     std::cout << "Game Over!!!\n";
+    std::cout << "Final Score: " << score << "\n";
 
     return 0;
 }
